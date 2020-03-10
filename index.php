dbf6993e45566dd0228b5896669047efb88947a2
Kit Tejido <tejidokit@gmail.com>
2020-03-09 23:50:05 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!Doctype html>
+<html>
+<head>
+    <title>watchee</title>
+
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css"
+</head>
+
+<body>
+
+    <div id="pageContainer">
+        
+        <div id="mastHeadContainer">
+            hello
+        </div>
+
+        <div id="sideNavContainer">
+
+        </div>
+
+        <div id="mainSectionContainer">
+
+        </div>
+
+    </div>
+
+</body>
+
+</html>
