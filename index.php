4824323023d991a929a3b4664a06153c778cca79
Kit Tejido <tejidokit@gmail.com>
2020-03-10 01:57:37 -0600

    added bootstrap, jquery, cdn

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <!Doctype html>
 <html>
 <head>
+
     <title>watchee</title>
 
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css"
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">  
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
 </head>
 
 <body>
@@ -19,6 +25,10 @@
         </div>
 
         <div id="mainSectionContainer">
+
+            <div id="mainContentContainer">
+
+            </div>
 
         </div>
 
