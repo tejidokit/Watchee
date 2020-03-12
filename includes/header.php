1efbe84421c46a9c852996382a9ef07941ca318b
Kit Tejido <tejidokit@gmail.com>
2020-03-11 20:46:16 -0600

    separated header/navbar and footer files from index.php

---
+++ includes/header.php
@@ -0,0 +1,58 @@
+<!Doctype html>
+<html>
+    <head>
+        <title>watchee</title>
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css"> 
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        
+        <script src="assets/js/commonActions.js"></script>
+    </head>
+
+<body>
+
+    <div id="pageContainer">
+        
+        <div id="mastHeadContainer">
+
+            <button class="navShowHide">
+                <img src="assets/images/icons/menu.png">
+            </button>
+
+            <a class="logoContainer" href="index.php">
+                <img src="assets/images/icons/WatcheeLogo.png" title="logo">
+            </a>
+
+            <div class="searchBarContainer">
+                <form action="search.php" method="GET">
+                    <input type="text" class="searchBar" name="term" placeholder="Search...">
+                    <button class="searchButton">
+                        <img src="assets/images/icons/search.png" alt="">
+                    </button>
+                </form>
+            </div>
+
+            <div class="rightIcons">
+                <a href="upload.php">
+                    <img class="upload" src="assets/images/icons/upload.png" alt="">
+                </a>
+                <a href="###">
+                    <img class="upload" src="assets/images/icons/default.png" alt="">
+                </a>
+            </div>
+
+        </div>
+
+        <div id="sideNavContainer" style="display:none;">
+
+        </div>
+
+        <div id="mainSectionContainer">
+
+            <div id="mainContentContainer">
+                hello
+            </div>
